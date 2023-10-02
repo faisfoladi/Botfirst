43d5df79e549f239b0d96275e9f26c99c71f9789
faisfoladi <116447863+faisfoladi@users.noreply.github.com>
2023-10-03 01:21:14 +0200

    Update Botz.py

--- Botz.py
+++ Botz.py
@@ -1,92 +1,81 @@
 import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-# Setzen Sie hier Ihren Telegram-Bot-Token ein
+# تنظیم توکن بات خود اینجا
 bot_token = '6023368456:AAHGUL4ZTfGAG7MK1CqF_0pCTryQaQMjmlU'
 
-# Benutzer-ID des Administrators (Ihre ID). Sie müssen Ihre eigene ID einsetzen.
-admin_id = '5948436434'
+# تنظیم متغیر برای ذخیره وضعیت سکوت کاربران
+silent_users = set()
 
-# Stadien für den Konversations-Handler
-START, MENU = range(2)
-
-# Benutzerdaten speichern
-user_data = {}
-
-# Initialisieren Sie den Updater mit Ihrem Bot-Token
+# مقداردهی ربات با توکن
 updater = Updater(token=bot_token, use_context=True)
+dispatcher = updater.dispatcher
 
-# Willkommensnachricht, die an neue Mitglieder gesendet wird
-welcome_message = "سلام چطوری خوشومدی به گروه خودت {mention} "
+# تنظیمات لاگینگ
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Funktion, um neue Mitglieder zu begrüßen
-def welcome_new_member(update: Update, context: CallbackContext) -> int:
-    message = update.message
-    new_members = message.new_chat_members
+# تابع برای خوش آمد گویی به کاربر جدید
+def welcome(update: Update, context: CallbackContext) -> None:
+    user = update.message.from_user
+    context.bot.send_message(chat_id=update.message.chat_id, text=f"سلام {user.first_name}! خوش آمدید به گروه.")
 
-    for member in new_members:
-        user_id = member.id
-        context.bot.send_message(chat_id=message.chat_id, text=welcome_message, reply_to_message_id=message.message_id)
-
-# Handler für neue Mitglieder
-new_member_handler = MessageHandler(Filters.status_update.new_chat_members, welcome_new_member)
-updater.dispatcher.add_handler(new_member_handler)
-
-# Funktion, um auf den Befehl /hallo zu reagieren
+# تابع برای جواب سلام دادن
 def say_hello(update: Update, context: CallbackContext) -> None:
-    context.bot.send_message(chat_id=update.effective_chat.id, text="سلام خوبی چه خبر؟ ")
+    user = update.message.from_user
+    context.bot.send_message(chat_id=update.message.chat_id, text=f"سلام {user.first_name}!")
 
-# Handler für den Befehl /hallo hinzufügen
-hello_handler = CommandHandler('سلام', say_hello)
-updater.dispatcher.add_handler(hello_handler)
+# تابع برای اخراج کردن کاربر
+def kick_user(update: Update, context: CallbackContext) -> None:
+    if update.message.from_user.id == ADMIN_USER_ID:  # تغییر ADMIN_USER_ID به شناسه شما
+        user_id = context.args[0] if context.args else None
+        if user_id:
+            context.bot.kick_chat_member(chat_id=update.message.chat_id, user_id=user_id)
+            context.bot.send_message(chat_id=update.message.chat_id, text=f"کاربر {user_id} از گروه اخراج شد.")
 
-# Funktion, um auf den Befehl /sperr zu reagieren und den Benutzer zu sperren
-def ban_user(update: Update, context: CallbackContext) -> int:
-    chat_id = update.message.chat_id
-    user_id = update.message.from_user.id
-
-    # Überprüfen, ob der ausführende Benutzer ein Gruppenadministrator ist
-    chat_member = context.bot.get_chat_member(chat_id, user_id)
-    
-    if chat_member.status == 'administrator' and str(user_id) == admin_id:
-        if len(context.args) == 1:
-            user_to_ban_id = context.args[0]
-            context.bot.kick_chat_member(chat_id=chat_id, user_id=user_to_ban_id)
-            context.bot.send_message(chat_id=chat_id, text=f"کاربر {user_to_ban_id} دفن شد.")
-        else:
-            context.bot.send_message(chat_id=chat_id, text="Verwendung: /sperr Benutzer_ID")
-    else:
-        context.bot.send_message(chat_id=chat_id, text="Sie haben keine Berechtigung, diesen Befehl auszuführen.")
-
-# Handler für den Befehl /sperr hinzufügen
-ban_handler = CommandHandler('بمیر', ban_user, pass_args=True)
-updater.dispatcher.add_handler(ban_handler)
-
-# Funktion, um den Benutzer stummzuschalten
+# تابع برای سکوت کاربر
 def mute_user(update: Update, context: CallbackContext) -> None:
-    chat_id = update.message.chat_id
+    if update.message.from_user.id == ADMIN_USER_ID:  # تغییر ADMIN_USER_ID به شناسه شما
+        user_id = context.args[0] if context.args else None
+        if user_id:
+            context.bot.restrict_chat_member(chat_id=update.message.chat_id, user_id=user_id, can_send_messages=False)
+            silent_users.add(user_id)
+            context.bot.send_message(chat_id=update.message.chat_id, text=f"کاربر {user_id} به حالت بی صدا درآمد.")
+
+# تابع برای آزاد کردن کاربر از اخراج و سکوت
+def unban_unmute_user(update: Update, context: CallbackContext) -> None:
+    if update.message.from_user.id == ADMIN_USER_ID:  # تغییر ADMIN_USER_ID به شناسه شما
+        user_id = context.args[0] if context.args else None
+        if user_id:
+            context.bot.unban_chat_member(chat_id=update.message.chat_id, user_id=user_id)
+            if user_id in silent_users:
+                context.bot.restrict_chat_member(chat_id=update.message.chat_id, user_id=user_id, can_send_messages=True)
+                silent_users.remove(user_id)
+            context.bot.send_message(chat_id=update.message.chat_id, text=f"کاربر {user_id} از اخراج و حالت بی صدا خارج شد.")
+
+# تابع برای پیام‌های بدون دستور
+def handle_messages(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
-    target_user_id = context.args[0] if context.args else None
+    if user_id in silent_users:
+        context.bot.send_message(chat_id=update.message.chat_id, text="شما به حالت بی صدا هستید.")
 
-    if target_user_id:
-        # Überprüfen, ob der ausführende Benutzer ein Gruppenadministrator ist
-        chat_member = context.bot.get_chat_member(chat_id, user_id)
-        
-        if chat_member.status == 'administrator' and str(user_id) == admin_id:
-            context.bot.restrict_chat_member(chat_id=chat_id, user_id=target_user_id, can_send_messages=False)
-            context.bot.send_message(chat_id=chat_id, text=f"Benutzer {target_user_id} امی نفر چوپ شد.")
-        else:
-            context.bot.send_message(chat_id=chat_id, text="Sie haben keine Berechtigung, diesen Befehl auszuführen.")
-    else:
-        context.bot.send_message(chat_id=chat_id, text="Verwendung: /stummsetzen Benutzer_ID")
-
-# Handler für den Befehl /stummsetzen hinzufügen
+# تعریف دستورات
+welcome_handler = MessageHandler(Filters.status_update.new_chat_members, welcome)
+hello_handler = CommandHandler('سلام', say_hello)
+kick_handler = CommandHandler('بمور', kick_user, pass_args=True)
 mute_handler = CommandHandler('چوپ', mute_user, pass_args=True)
-updater.dispatcher.add_handler(mute_handler)
+unban_unmute_handler = CommandHandler('آزاد', unban_unmute_user, pass_args=True)
+messages_handler = MessageHandler(Filters.text & ~Filters.command, handle_messages)
 
-# Fügen Sie hier weitere Funktionen wie /info, /beitrag, /admin hinzu...
+# افزودن دستورات به دیسپچر
+dispatcher.add_handler(welcome_handler)
+dispatcher.add_handler(hello_handler)
+dispatcher.add_handler(kick_handler)
+dispatcher.add_handler(mute_handler)
+dispatcher.add_handler(unban_unmute_handler)
+dispatcher.add_handler(messages_handler)
 
-# Starten Sie den Bot
+# شروع ربات
 updater.start_polling()
 updater.idle()
+
